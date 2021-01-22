d7bb54f63a47df7367b9552d23ce1b9ea9fdf3cb
Michelle Tai <michelle.tai@duke.edu>
2021-01-22 12:27:08 -0500

    seems finished, but gettingerror that my Yelp import in App isn't working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,29 +3,7 @@ import './App.css';
 import logo from '../../logo.svg';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
-
-
-let business = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-let businesses = [
-    business, 
-    business, 
-    business, 
-    business, 
-    business, 
-    business
-];
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
     searchYelp(term, location, sortBy){
